b9447b20c6c018f7231a7376f325a13187840a0c
Ownerclone <info@ownerclone.com>
2026-01-18 19:32:20 -0500

    Update ScreenplayEditor.tsx

--- components/editor/ScreenplayEditor.tsx
+++ components/editor/ScreenplayEditor.tsx
@@ -416,7 +416,7 @@ export default function ScreenplayEditor({
             onClick={addNewElement}
             className="mx-4 mt-4 px-4 py-2 text-sm text-gray-500 hover:text-gray-700 hover:bg-gray-50 rounded border-2 border-dashed border-gray-300"
           >
-+ Add element
+          + Add element
           </button>
         </div>
       </div>
