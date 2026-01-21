f20c6845363de9878dfc6df3bb179d6dcc436356
Ownerclone <info@ownerclone.com>
2026-01-21 12:01:38 -0500

    Create page.tsx

---
+++ app/cms/blog/new/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function BlogNewRedirect() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.replace('/cms/blogposts/new');
+  }, [router]);
+
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div className="text-gray-500">Redirecting...</div>
+    </div>
+  );
+}
